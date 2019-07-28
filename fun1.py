ee54e3cdbce295e30031d0c81de8949d2223df83
ahmadf21-meet <ahmadf21@meet.mit.edu>
2019-07-28 11:28:40 -0400

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,9 @@
+
+def add_numbers():
+    #write the body of this
+    #function, similar to the block
+    #of code we just saw. Hint:
+    #don’t forget to use return
+
+answer = add_numbers()
+print(answer)
